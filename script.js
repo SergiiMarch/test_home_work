b2232ec6a49946e65e1c9709404dbacf8ea00ec2
Sergii Marchenko <marikmarchelo@gmail.com>
2023-07-15 23:50:06 +0300

    new my commit

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
-const scrollHendler = () => {
-  console.log("scroll");
-};
+const productList = [
+  {
+    id: 1,
+    text: "молоко",
+  },
+  {
+    id: 2,
+    text: "хліб",
+  },
+  {
+    id: 3,
+    text: "кава",
+  },
+  {
+    id: 4,
+    text: "мед",
+  },
+  {
+    id: 5,
+    text: "виноград",
+  },
+];
 
-addEventListener("scroll", scrollHendler);
+// const renderList = (products) =>
+//   products.map(({ id, text }) => `<li id=${id}>${text}</li>`).join("");
+
+const renderList = (products) =>
+  products.reduce((acc, { id, text }) => acc + `<li id=${id}>${text}</li>`, "");
+
+const addListItems = (string) => {
+  const ul = document.querySelector(".list");
+  ul.insertAdjacentHTML("beforeend", string);
+};
+// addListItems(renderList(productList));
+const result = renderList(productList);
+addListItems(result);
