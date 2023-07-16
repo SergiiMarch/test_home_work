ed7a9e42040413e1433c2e37946ff3ec39bd6837
Sergii Marchenko <marikmarchelo@gmail.com>
2023-07-16 11:44:47 +0300

    New commit

--- script.js
+++ script.js
@@ -23,9 +23,13 @@ const productList = [
 
 // const renderList = (products) =>
 //   products.map(({ id, text }) => `<li id=${id}>${text}</li>`).join("");
+const formEl = document.querySelector(".form");
+const inputEl = document.querySelector(".input");
+
+const creatLi = (id, text) => `<li id=${id}>${text}</li>`;
 
 const renderList = (products) =>
-  products.reduce((acc, { id, text }) => acc + `<li id=${id}>${text}</li>`, "");
+  products.reduce((acc, { id, text }) => acc + creatLi(id, text), "");
 
 const addListItems = (string) => {
   const ul = document.querySelector(".list");
@@ -34,3 +38,11 @@ const addListItems = (string) => {
 // addListItems(renderList(productList));
 const result = renderList(productList);
 addListItems(result);
+
+const submitHandler = (e) => {
+  e.preventDefault();
+  addListItems(creatLi(inputEl.value, inputEl.value));
+  inputEl.value = "";
+};
+
+formEl.addEventListener("submit", submitHandler);
